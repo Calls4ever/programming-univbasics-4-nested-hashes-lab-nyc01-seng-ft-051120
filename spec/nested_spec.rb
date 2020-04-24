68d8465b87482b85e1c3f0be2bb43952982ac8cf
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-24 22:22:33 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -21,7 +21,7 @@ describe "Working with Nested Hashes -" do
     end
   end
 
-  describe "the adding_matz method" do 
+  describe "the adding_matz method" do
     it "operates on the programmer_hash and adds a key/value pair to the top level of the hash, returning the newly-added-to hash" do
       expect(adding_matz.keys).to include(:yukihiro_matsumoto)
     end
@@ -29,7 +29,7 @@ describe "Working with Nested Hashes -" do
 
   describe "the changing_alan method" do
     it "operates on the programmer_hash and changes what Alan Kay is known for, returning the newly-changed hash" do
-      expect(changing_alan[:alan_kay][:known_for]).to eq("GUI")
+      expect(changing_alan[:alan_kay][:known_for]).to eq("Object Orientation")
     end
   end
 
